45528baca3149ad84b9c6e59329b6e83c27806b4
Kunwoo Ahn <dryrain@fastmail.com>
2022-03-31 11:11:53 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
+# command line git tutorial
+# 안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕 
+# hihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihi
+# 재미있는 깃허브 실습 수업
+
 print("Hello world")
 print("Tell your world")
 print("Tell his world")
